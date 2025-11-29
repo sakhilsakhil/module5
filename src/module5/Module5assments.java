899da313686d3d0813df8bee93571380bdcf698f
sakhilsakhil <sakhilpurathur@gmai.com>
2025-11-29 18:07:06 +0300

    Commit3

--- src/module5/Module5assments.java
+++ src/module5/Module5assments.java
@@ -2,12 +2,12 @@ package module5;
 
 public class Module5assments {
 
-	int i;
+	int i=0;
 	public static void main(String[] args) {
 		System.out.println("The assement");
 		for(i=0;i<=5;i++)
 		{
-			println("i",i);
+			System.out.println("i="+i);
 		}
 
 	}
